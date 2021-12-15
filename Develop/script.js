eff4ffdcb64b3589793e538b6544dbbbae8b985f
perfect perfect <gilberto.j.covarrubias@gmail.com>
2021-12-15 13:25:21 -0500

    adding to github

---
+++ Develop/script.js
@@ -0,0 +1,26 @@
+// Assignment code here
+debugger;
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Create generatePassword(); function/variable
+var generatePassword = function () {
+  window.prompt("i wish i was dead");
+};
+
+// Write password to the #password input
+
+var writePassword = function() {
+  debugger;
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+};
+
+
+// Add event listener to generate button
+//when we click the button it runs the function writePassword
+generateBtn.addEventListener("click", writePassword);
