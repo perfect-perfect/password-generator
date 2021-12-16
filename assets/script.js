67a911ec25ce59bc00e8512075ea3c8fe68d571b
perfect perfect <gilberto.j.covarrubias@gmail.com>
2021-12-15 20:21:33 -0500

    no sucess. cannot figure how to run for loop and rutrn information to writepassword function

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,22 @@
 // Assignment code here
-debugger;
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+//var passWordFunction = function () {
 
 // Create generatePassword(); function/variable
 var generatePassword = function () {
-  window.prompt("input here");
+
+  for (i = 0; i > passwordInfo.length; i++) {
+
+    console.log(i)
+  }
+
+
+  //I am having the same issue i was having eariler i cannot get anything from inside this function to go into the other functions.
+  //var passwordFunction = window.prompt("input here");
+  //return passwordFunction;
 };
 
 
@@ -14,13 +24,37 @@ var generatePassword = function () {
 // Write password to the #password input
 
 var writePassword = function() {
-  debugger;
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
 };
+
+
+var getPasswordLength = function() {
+  var length = "";
+
+  while (length === "" || length === null || length < 8 || length > 128 ) {
+    length = prompt("Choose a length between 8 and 128")
+
+    // convert answer into an interger, because prompts are automatically strings
+    length = parseInt(length);
+  }
+  
+  // convert answer into an interger, because prompts are automatically strings
+  return length;
+};
+
+var passwordInfo = {
+  length: getPasswordLength (),
+  //window.prompt("Choose a length between 8 - 128 characters."),
+  //lowercase: window.confirm("Include lowercase letters?"),
+  //uppercase: window.confirm("include uppercase letters?"),
+  //numeric: window.confirm("include numeric values?"),
+  //special: window.confirm("include special characters?")
+}
+
 
 
 // Add event listener to generate button
