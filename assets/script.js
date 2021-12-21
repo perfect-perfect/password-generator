2fb0d7eaf5c4514a93f67ce7a440a4b55f7d062d
perfect perfect <gilberto.j.covarrubias@gmail.com>
2021-12-20 23:31:23 -0500

    after first tutoring session

--- assets/script.js
+++ assets/script.js
@@ -1,68 +1,91 @@
 // Assignment code here
 
 
-// Get references to the #generate element
+// This creates a DOM representation of the html element with id="generate". In this case the <button> element
 var generateBtn = document.querySelector("#generate");
-//var passWordFunction = function () {
+console.log(generateBtn);
 
-var selectCharacters = function (selectedCharacter) {
-  character
-}
+function promptUser() {
+  var length = window.prompt("Choose a length between 8 - 128 characters.");
+  if (length < 8 || length >128 || length === "" || length === null) {
+    window.alert("You must provide a number value betwwen 8 and 128")
 
-// Create generatePassword(); function/variable
-var generatePassword = function () {
+    return null;
+  }
+  var lowercase = confirm("Include lowercase letters?");
+  var uppercase = confirm("include uppercase letters?");
+  var numeric = window.confirm("include numeric values?");
+  var special = window.confirm("include special characters?");
 
-  for (i = 0; i > passwordInfo.length; i++) {
-
-    selectCharacter();
-
-    console.log(i)
+  if (lowercase === false && uppercase === false && numeric === false && special === false) {
+    alert("The password must contain at least one type of character")
+    return null;
+  }
+  var passwordInfo = {
+    //I have to convert the string value that results from the prompt into a numeric value
+    length,
+    lowercase,
+    uppercase,
+    numeric,
+    special
   }
 
+  return passwordInfo;
+}
+function random(array){
+  // use math.floor and math.random to geta random index 
+  // use random index to pick element out of array[randomeindex]
+}
+// create generatePassword() function here
+function generatePassword() {
+  var passwordObj = promptUser();
+  console.log(passwordObj);
+  var possibleChar = [];
+  var finalPass = []
 
-  //I am having the same issue i was having eariler i cannot get anything from inside this function to go into the other functions.
-  //var passwordFunction = window.prompt("input here");
-  //return passwordFunction;
+  if(passwordObj.uppercase === true){
+   possibleChar = possibleChar.concat(upperArr)
+  }
+  for (let i = 0; i < passwordObj.length; i++) {
+    var randomChar = random(possibleChar)
+    finalPass.push(randomChar)
+  }
+  return finalPass.join("")
+  // What i create here will ultimately be sent to writePassword and set equal to the variable password
+  //for (var i = 0; i < passwordInfo.length; i++)
+    // this needs to go in the object maybe
+    //if (passwordInfo.length < 8 || passwordInfo.length >128 || passwordInfo.length = "" || passwordInfo.length = null) {
+    //  window.alert("You must provide a number value betwwen 8 and 128")
+
+    
+
+    //}
+    // (passwordInfo.lowercase) {
+      //code to select a lower case letter from an array
+    //}
+  // Do I need to do a return here?
+  // having some issue understaning what we do at the end of functions. seems lots of different options. struggling to understand the end of function conceptually.
 };
-
-
-
 // Write password to the #password input
-
-var writePassword = function() {
+function writePassword() {
   var password = generatePassword();
+  // creates a DOM object of the html element with an id="password". in this case <textarea> element
   var passwordText = document.querySelector("#password");
-
+  // this sets the DOM object's .value method to the password we just created
   passwordText.value = password;
 
-};
-
-
-var getPasswordLength = function() {
-  var length = "";
-
-  while (length === "" || length === null || length < 8 || length > 128 ) {
-    length = prompt("Choose a length between 8 and 128")
-
-    // convert answer into an interger, because prompts are automatically strings
-    length = parseInt(length);
-  }
-  
-  // convert answer into an interger, because prompts are automatically strings
-  return length;
-};
-
-var passwordInfo = {
-  length: getPasswordLength (),
-  //window.prompt("Choose a length between 8 - 128 characters."),
-  //lowercase: window.confirm("Include lowercase letters?"),
-  //uppercase: window.confirm("include uppercase letters?"),
-  //numeric: window.confirm("include numeric values?"),
-  //special: window.confirm("include special characters?")
 }
 
+// create array of characters here
+
+var upperArr = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","Z"
+]
 
 
-// Add event listener to generate button
-//when we click the button it runs the function writePassword
+
+
+
+
+// This event listener listens for clicks in the generateBtn area, which is the <button> element
 generateBtn.addEventListener("click", writePassword);
