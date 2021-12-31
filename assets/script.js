98ea65008a6aea55e1c4b28735827b8178d427e1
perfect perfect <gilberto.j.covarrubias@gmail.com>
2021-12-31 15:39:40 -0500

    finished randomizer function and updated special characrers array

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Creates the arrays I will be pulling from
 var upperArr = [
   "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Y","Z"
 ]
@@ -8,16 +8,18 @@ var numericArr = [
 ]
 
 var specialArr = [
-  "!","#","$","%","*","+","/",":",";","?","@","^","_","`","~"
+  "!","#","$","%","*","+","/",":",";","?","@","^","_","`","~","&","(",")","-",".","<","=",">","{","}","|"
 ]
 
 // This creates a DOM representation of the html element with id="generate". In this case the <button> element
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
+// console.log(generateBtn);
 
-function promptUser() {
-  var length = window.prompt("Choose a length between 8 - 128 characters.");
-  if (length < 8 || length >128 || length === "" || length === null) {
+var promptUser = function() {
+  var passwordLength = window.prompt("Choose a length between 8 - 128 characters.");
+  parseInt(passwordLength);
+  // console.log(length)
+  if (passwordLength < 8 || passwordLength >128 || passwordLength === "" || passwordLength === null) {
     window.alert("You must provide a number value between 8 and 128")
 
     return null;
@@ -42,50 +44,68 @@ function promptUser() {
 
   return passwordInfo;
 }
-function random(array){
-  if (!array.isArray()) {
-    alert("There was an error with random generator")
-  }
+
+var randomizer = function(array) {
+  //function random(array){
+  //if (!array.isArray()) {
+  //  alert("There was an error with random generator")
+  //}
   var randomIndex = Math.floor(Math.random() * array.length);
 
   var randomEl = array[randomIndex];
+
   return randomEl;
 
   // use math.floor and math.random to geta random index 
   // use random index to pick element out of array[randomeindex]
 }
+
+
 // create generatePassword() function here
 function generatePassword() {
   var passwordObj = promptUser();
-  console.log(passwordObj);
+  // console.log(passwordObj);
   var possibleChars = [];
   var finalPass = []
 
+  // LOWERCASE
   if(passwordObj.lowercase === true){
   //             upperArr.map(function(upperChar){upperChar.toLowerCase()})
-  let lowerArr = upperArr.map(upperChar => upperChar.toLowerCase())
+    var lowerArr = upperArr.map(upperChar => upperChar.toLowerCase())
 
-   possibleChars = possibleChars.concat(lowerArr)
+    possibleChars = possibleChars.concat(lowerArr)
   }
+  console.log(possibleChars);
 
+  // UPPERCARE
   if(passwordObj.uppercase === true){
     possibleChars = possibleChars.concat(upperArr)
   }
+  console.log(possibleChars);
 
+
+  // NUMERIC
   if (passwordObj.numeric === true) {
     possibleChars = possibleChars.concat(numericArr)
     
   }
+  console.log(possibleChars);
 
+
+  // SPECIAL CHARACTERS
   if (passwordObj.special === true) {
     possibleChars = possibleChars.concat(specialArr)
 
   }
-
+  console.log(possibleChars);
+  console.log(passwordObj);
+  // PASSWORD LENGTH
   for (let i = 0; i < passwordObj.passwordLength; i++) {
-    var randomChar = random(possibleChars);
+    var randomChar = randomizer(possibleChars);
     finalPass.push(randomChar);
+    console.log(finalPass);
   }
+  
 
   return finalPass.join("")
   
